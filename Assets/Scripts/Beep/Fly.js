a673f18a55abe985d0d94578558e34e76a36f5e7
Jonathan Rodriguez <jonathanrodriguez@Jonathans-MacBook-Pro.local>
2017-01-27 22:27:50 +0100

    First commit

---
+++ Assets/Scripts/Beep/Fly.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+public var velocity : float = 0.0;
+public var maxDistance : float = 0.0;
+public var goBack : boolean = false;
+
+private var originalPositionX : float;
+private var nextPosition : float;
+
+private var maxDistanceXPositive : float;
+private var maxDistanceXNegative : float;
+
+function Start() {
+	originalPositionX = parseFloat(transform.position.x);
+	
+	maxDistanceXPositive = originalPositionX + maxDistance;
+	maxDistanceXNegative = originalPositionX - maxDistance;
+	nextPosition = originalPositionX + velocity;
+	if (goBack) velocity = velocity * -1;
+}
+
+function FixedUpdate () {
+	if (!goBack) {
+		if (nextPosition <= maxDistanceXPositive) {
+			transform.position = new Vector3(nextPosition, transform.position.y, 0);
+		} else { 
+			goBack = true;
+			velocity = velocity * -1;
+		}
+
+	} else {
+		if (nextPosition >= maxDistanceXNegative) {
+			transform.position = new Vector3(nextPosition, transform.position.y, 0);
+		} else {
+			goBack = false;
+			velocity = velocity * -1;
+		}
+	}
+
+	nextPosition += velocity;
+}
+
+
---
+++ Assets/Scripts/Collision.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function OnCollisionEnter2D (coll: Collision2D) {
+	Debug.Log(coll.gameObject.tag);
+}
